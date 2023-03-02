98b01a2a98d93b34129a0bab946541f8295f01ff
Sergei Mikhailenko <sersus1991@gmail.com>
2023-03-02 14:12:54 +0200

    contactForm with hooks

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,62 +1,61 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 import { Box } from 'components/Common/Box';
 import { Label, Input, Button } from './ContactForm.styled';
 import { validation } from 'constants';
 
-export class ContactForm extends Component {
-  state = {
-    name: '',
-    number: '',
-  };
+export const ContactForm = ({ onSubmit }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    const { name, number } = this.state;
-    this.props.onSubmit({ name, number });
-    this.resetState();
+    onSubmit({ name, number });
+    resetState();
   };
 
-  handleChange = e => {
+  const handleChange = e => {
     const { name, value } = e.target;
-    this.setState(prev => ({ ...prev, [name]: value }));
+    if (name === 'name') setName(value);
+    if (name === 'number') setNumber(value);
+    // setState(prev => ({ ...prev, [name]: value }));
   };
 
-  resetState = () => this.setState({ name: '', number: '' });
+  const resetState = () => {
+    setName('');
+    setNumber('');
+  };
 
-  render() {
-    const { name, number } = this.state;
-    const { handleSubmit, handleChange } = this;
-    return (
-      <Box as="form" mb={4} onSubmit={handleSubmit}>
-        <Label>
-          Name
-          <Input
-            type="text"
-            name="name"
-            value={name}
-            pattern={validation.text.name}
-            title={validation.text.title}
-            onChange={handleChange}
-          />
-        </Label>
-        <Label>
-          Number
-          <Input
-            type="tel"
-            name="number"
-            value={number}
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title={validation.tel.title}
-            required
-            onChange={handleChange}
-          />
-        </Label>
-        <Button type="submit">Add contact</Button>
-      </Box>
-    );
-  }
-}
+  return (
+    <Box as="form" mb={4} onSubmit={handleSubmit}>
+      <Label>
+        Name
+        <Input
+          type="text"
+          name="name"
+          value={name}
+          pattern={validation.text.name}
+          title={validation.text.title}
+          onChange={handleChange}
+        />
+      </Label>
+      <Label>
+        Number
+        <Input
+          type="tel"
+          name="number"
+          value={number}
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title={validation.tel.title}
+          required
+          onChange={handleChange}
+        />
+      </Label>
+      <Button type="submit">Add contact</Button>
+    </Box>
+  );
+};
 
 ContactForm.propTypes = {
   onSubmit: PropTypes.func,
